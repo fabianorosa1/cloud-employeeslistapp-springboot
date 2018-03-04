5a017f4143e167d758a23326294dfdae8453bd4a
fabianorosa <fabianorosa@fabianosmacbook.home>
2018-03-04 14:33:44 +0000

    Add API call documentation

--- java/src/main/java/com/sap/cf/springboot/controller/EmployeeController.java
+++ java/src/main/java/com/sap/cf/springboot/controller/EmployeeController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sap.cf.springboot.model.Employee;
 import com.sap.cf.springboot.service.IEmployeeService;
+import com.sap.xs2.security.container.SecurityContext;
+import com.sap.xs2.security.container.UserInfo;
+import com.sap.xs2.security.container.UserInfoException;
 
 /**
  * 
@@ -50,7 +53,7 @@ public class EmployeeController {
 	@GetMapping("/employee/{id}")
 	@ResponseBody
 	public Employee getEmployeeById(@PathVariable(value = "id") final Integer id) {
-		logger.info("GET Employee BY ID: " + id);
+		logger.info(">>> GET Employee BY ID: " + id);
 		
 		return employeeService.findOneById(id);
 	}
@@ -58,9 +61,35 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	@ResponseBody
 	public List<Employee> getAllEmployees() {
-		logger.info("GET ALL Employee");
+		logger.info(">>> GET ALL Employee");
 		List<Employee> listEmployee = employeeService.findAll();
 		
 		return listEmployee;
 	}	
+	
+	/*
+	 * https://help.sap.com/viewer/65de2977205c403bbc107264b8eccf4b/Cloud/en-US/be97ec4a799c4135884c62610fea2a8f.html
+	 */
+	@GetMapping("/info")
+	@ResponseBody
+	public String getLoginInfo() throws UserInfoException {
+		logger.info(">>> getLoginInfo");
+		
+		UserInfo userInfo = SecurityContext.getUserInfo(); 
+		logger.info("userInfo: " + userInfo);
+		
+		String name = userInfo.getLogonName(); 
+		logger.info("name: " + name);
+		
+		String email = userInfo.getEmail();
+		logger.info("email: " + email);
+		
+		//String[] attribute = userInfo.getAttribute("my attribute");
+		//logger.info("attribute: " + attribute);
+		
+		boolean hasDeleteScope = userInfo.checkLocalScope("Delete");
+		logger.info("hasDeleteScope: " + hasDeleteScope);
+		
+		return name;
+	}
 }
